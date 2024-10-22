1a71d4feddb9c2c827c516bf173f8caca4820148
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-10-23 01:11:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-	arr.sort();
-let res = [];
-let sum = arr[0];
-arr.map((currEle,index) => {
-  if(index>=1){
-  sum+=currEle;
-  res.push(sum);
-  }
-})
-let ans = 0 ;
-res.forEach((currEle) => {
-  ans += currEle;
-})
+	let sum = 0;
+let ans = 0;
+
+
+while(arr.length!=1){
+	arr.sort(function(a,b){return a-b});
+	sum = arr[0]+arr[1]
+	ans += sum
+	arr.shift()
+	arr.shift()
+	arr.push(sum)
+	
+	
+}
 return ans;
+	
+
   
 }
 
